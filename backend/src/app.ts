963fe0e1e52d66d8a313fc5cdb38a000121e77af
Juan Comes <juancomes54@gmail.com>
2026-09-08 15:17:53 -0300

    chore: initialize StockFlow

---
+++ backend/src/app.ts
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000;
+
+app.get("/api/health", (req, res) => {
+  res.json({
+    status: "ok"
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+});
