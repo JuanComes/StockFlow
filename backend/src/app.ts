32bf8a610f30325d9152a19451e9de605440036d
Juan Comes <juancomes54@gmail.com>
2026-09-08 18:14:13 -0300

    feat: improve backend error handling

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from "express";
+import { pool } from "./db/database.js";
 
 const app = express();
 
 const PORT = 3000;
+
+// Middleware
+app.use(express.json());
 
 app.get("/api/health", (req, res) => {
   res.json({
@@ -10,6 +14,198 @@ app.get("/api/health", (req, res) => {
   });
 });
 
+app.get("/api/test-db", async (req, res, next) => {
+  try {
+    const result = await pool.query("SELECT NOW()");
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/products", async (req, res, next) => {
+  try {
+    const result = await pool.query(`
+      SELECT
+        products.id,
+        products.name,
+        products.sku,
+        products.sale_price,
+        products.stock,
+        categories.name AS category
+      FROM products
+      JOIN categories
+        ON products.category_id = categories.id
+    `);
+
+    res.json(result.rows);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.post("/api/products", async (req, res, next) => {
+  try {
+    const {
+      name,
+      description,
+      sku,
+      purchase_price,
+      sale_price,
+      stock,
+      minimum_stock,
+      category_id
+    } = req.body;
+
+    const result = await pool.query(
+      `
+        INSERT INTO products (
+          name,
+          description,
+          sku,
+          purchase_price,
+          sale_price,
+          stock,
+          minimum_stock,
+          category_id
+        )
+        VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
+        RETURNING *
+      `,
+      [
+        name,
+        description,
+        sku,
+        purchase_price,
+        sale_price,
+        stock,
+        minimum_stock,
+        category_id
+      ]
+    );
+
+    res.status(201).json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.get("/api/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      `
+        SELECT
+          products.id,
+          products.name,
+          products.description,
+          products.sku,
+          products.purchase_price,
+          products.sale_price,
+          products.stock,
+          products.minimum_stock,
+          categories.name AS category
+        FROM products
+        JOIN categories
+          ON products.category_id = categories.id
+        WHERE products.id = $1
+      `,
+      [id]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+
+app.put("/api/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const {
+      name,
+      description,
+      sku,
+      purchase_price,
+      sale_price,
+      stock,
+      minimum_stock,
+      category_id
+    } = req.body;
+
+    const result = await pool.query(
+      `
+        UPDATE products
+        SET
+          name = $1,
+          description = $2,
+          sku = $3,
+          purchase_price = $4,
+          sale_price = $5,
+          stock = $6,
+          minimum_stock = $7,
+          category_id = $8
+        WHERE id = $9
+        RETURNING *
+      `,
+      [
+        name,
+        description,
+        sku,
+        purchase_price,
+        sale_price,
+        stock,
+        minimum_stock,
+        category_id,
+        id
+      ]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.delete("/api/products/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+
+    const result = await pool.query(
+      `
+        DELETE FROM products
+        WHERE id = $1
+        RETURNING *
+      `,
+      [id]
+    );
+
+    res.json(result.rows[0]);
+  } catch (error) {
+    next(error);
+  }
+});
+
+app.use(
+  (
+    err: any,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.error(err);
+
+    res.status(500).json({
+      error: "Error interno del servidor"
+    });
+  }
+);
+
+
 app.listen(PORT, () => {
-  console.log(`Servidor ejecutandose en http://localhost:${PORT}`);
+  console.log(`Servidor ejecutándose en http://localhost:${PORT}`);
 });
