18e1a501f53d10f451341fb0e817a4c059a3ad9c
Juan Comes <juancomes54@gmail.com>
2026-09-15 20:20:07 -0300

    feature: getProductById

--- frontend/src/services/product.ts
+++ frontend/src/services/product.ts
@@ -1,7 +1,15 @@
 export async function getProducts() {
-    const response = await fetch("http://localhost:3000/api/products");
+  const response = await fetch("http://localhost:3000/api/products");
 
-    const data = await response.json();
-    
-    return data;
+  const data = await response.json();
+
+  return data;
+}
+
+export async function getProductById(id: number) {
+  const response = await fetch(`http://localhost:3000/api/products/${id}`);
+
+  const data = await response.json();
+
+  return data;
 }
