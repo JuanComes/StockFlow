0f0f79b45901ee85b00b45fb4f86249c3552daa8
Juan Comes <juancomes54@gmail.com>
2026-09-24 17:09:49 -0300

    feat: include product name in stock movements

--- frontend/src/interfaces/StockMovement.ts
+++ frontend/src/interfaces/StockMovement.ts
@@ -1,7 +1,8 @@
 export interface StockMovement {
   id: number;
   product_id: number;
+  product_name: string;
   type: "IN" | "OUT";
   quantity: number;
-  purchase_price?: number;
+  purchase_price: number;
 }
